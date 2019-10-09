9542f028fd5289beaeba5fe8cef43d6d62f48790
brunopeople <brunopessoa.melo@gmail.com>
2019-10-08 21:24:46 -0300

    verificando os erros

--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -1,5 +1,4 @@
 // product.route.js
-
 const express = require('express');
 const app = express();
 const productRoutes = express.Router();
@@ -7,19 +6,19 @@ const productRoutes = express.Router();
 // Require Product model in our routes module
 let Product = require('../models/Product');
 
-// Defined store route
+// Define a rota loja
 productRoutes.route('/add').post(function (req, res) {
   let product = new Product(req.body);
   product.save()
     .then(product => {
-      res.status(200).json({ 'Product': 'Product has been added successfully' });
+      res.status(200).json({ 'Produto: O Produto foi adicionado com sucesso' });
     })
     .catch(err => {
-      res.status(400).send("unable to save to database");
+      res.status(400).send("não foi possível ");
     });
 });
 
-// Defined get data(index or listing) route
+// Defined a listagem do produtos criados(get, list)
 productRoutes.route('/').get(function (req, res) {
   Product.find(function (err, products) {
     if (err) {
@@ -31,7 +30,7 @@ productRoutes.route('/').get(function (req, res) {
   });
 });
 
-// Defined edit route
+// Defina a edição de rotas
 productRoutes.route('/edit/:id').get(function (req, res) {
   let id = req.params.id;
   Product.findById(id, function (err, product) {
@@ -39,21 +38,21 @@ productRoutes.route('/edit/:id').get(function (req, res) {
   });
 });
 
-//  Defined update route
+//  Defina a rota de atualização 
 productRoutes.route('/update/:id').post(function (req, res) {
   Product.findById(req.params.id, function (err, product) {
     if (!product)
-      res.status(404).send("Record not found");
+      res.status(404).send("Registro não encontrado!!");
     else {
       product.ProductName = req.body.ProductName;
       product.ProductDescription = req.body.ProductDescription;
       product.ProductPrice = req.body.ProductPrice;
 
       product.save().then(product => {
-        res.json('Update complete');
+        res.json('Atualização registrada!');
       })
         .catch(err => {
-          res.status(400).send("unable to update the database");
+          res.status(400).send("Não pode registrar a atualização pelo banco de dados");
         });
     }
   });
@@ -63,7 +62,7 @@ productRoutes.route('/update/:id').post(function (req, res) {
 productRoutes.route('/delete/:id').get(function (req, res) {
   Product.findByIdAndRemove({ _id: req.params.id }, function (err, product) {
     if (err) res.json(err);
-    else res.json('Successfully removed');
+    else res.json('Remoção feita com sucesso!!');
   });
 });
 
